869d5c55c6e70f0d73bff161d45455bb45a03715
nandaks <nandance07@gmail.com>
2024-12-04 11:15:27 +0530

    push  Jira001

--- Tests/test_webpage.py
+++ Tests/test_webpage.py
@@ -20,9 +20,9 @@ class TestMmt:
             TestMmt.counts += n.filtering()
             n.avilability_of_seats(TestMmt.counts)
 
-        except BaseException as msg:
+        except BaseException as msgg:
             td = datetime.datetime.now()
             name = f'Screenshot_{td.date}_{td.hour}_{td.minute}_{td.second}.png'
             path = "../Screenshots/"
             driver.save_screenshot(path + name)
-            raise msg
+            raise msgg
